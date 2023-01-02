de75851af19294ea2cd5869031e1cafb3b1cf000
FacundoOrcajo <facundo20orcajo@gmail.com>
2023-01-02 01:20:01 -0300

    Agregué archivos

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,47 @@
+const db = require('../database/models');
+
+module.exports = {
+    list : (req, res) => {
+        db.Movie.findAll()
+            .then((movies) => {
+                return res. send(movies)
+            })
+            .catch((error) => console.log(error))
+    },
+    detail : (req, res)=>{
+        db.Movie.findByPk(req.params.id)
+        .then((movie)=>{
+            return res.render('moviesDetail', {
+                movie
+            })
+        })
+        .catch((error) => console.log(error))
+    },
+    new : (req, res)=>{
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ]
+        })
+        .then((movies)=>{
+            return res.render('newestmovies', {
+                movies
+            })
+        })
+        .catch((error) => console.log(error))
+    },
+    recomended:(req, res)=>{
+        db.Movie.findAll({
+            order: [
+                ['rating', 'DESC']
+            ],
+            limit: 5 
+        })
+        .then((movies)=>{
+            return res.render('recommendedMovies', {
+                movies
+            })
+        })
+        .catch(error => console.log(error))
+    },
+}
